4a716eed8d6a67a8ef3be3d121af210ac5874a3c
Priyansh Dimri <priyanshd0908@gmail.com>
2025-03-09 10:33:14 +0530

    add Navbar.jsx and font-faces in global.css
    - Renamed Inter font file names

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./styles/global.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./pages/routes";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const router = createBrowserRouter(routes);
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,13 @@
 function Navbar() {
-  return <></>;
+  return (
+    <div id="navbar">
+      <div id="title">HashCart</div>
+      <div id="cart">
+        <div id="cart-count"></div>
+        <img src="./../assets/images/cart.svg" alt="Shopping Cart" />
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
