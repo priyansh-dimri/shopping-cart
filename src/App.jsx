5f3bd7d08cc9c2f64fa6ffa1adfef20cf80e3d3c
Priyansh Dimri <priyanshd0908@gmail.com>
2025-03-10 16:32:02 +0530

    add useEffect for fetching products and useStates

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,32 @@ import "./styles/global.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) =>
+        setProducts(
+          data.map((product) => ({
+            id: product.id,
+            title: product.title,
+            price: product.price,
+            image: product.image,
+          }))
+        )
+      )
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
+
   return (
     <div id="container">
-      <Navbar />
+      <Navbar cartCount={cartCount} />
       <main>
         <Outlet />
       </main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import { NavLink } from "react-router-dom";
 import CartIcon from "./../assets/images/cart.svg";
 
-function Navbar() {
+function Navbar({ cartCount }) {
   return (
     <div id="navbar">
       <div id="title">HashCart</div>
       <NavLink to="/" className="nav-link">
         Home
       </NavLink>
-      <NavLink to="/shop" className="nav-link">Shop</NavLink>
+      <NavLink to="/shop" className="nav-link">
+        Shop
+      </NavLink>
       <NavLink to="/cart" id="cart" type="button">
-        <div id="cart-count"></div>
+        <div id="cart-count">{cartCount > 99 ? "99+" : cartCount}</div>
         <img src={CartIcon} alt="Shopping Cart" className="icon" />
       </NavLink>
     </div>
